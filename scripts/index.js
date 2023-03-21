25742d0665a4f630bbc6d38c5a9b08a4bc379de8
Artmo0z <117703522+Artmo0z@users.noreply.github.com>
2023-03-21 15:32:57 +0700

    Project-5 Fixed v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,51 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
-
+const profileEditPopup = document.querySelector('.popup_edit-profile');
 const nameInput = document.querySelector('.popup__input_name_value');
 const jobInput = document.querySelector('.popup__input_job_value');
-
 const popup = document.querySelector('.popup');
 const formElement = popup.querySelector('.popup__form');
+const closePopupButton = profileEditPopup.querySelector('.popup__close');
+const addPopupPhoto = document.querySelector('.popup_addPhoto');
+const addButtonPhoto = document.querySelector('.profile__add-button');
+const closePopupAddPhotoButton = addPopupPhoto.querySelector('.popup__close-addphoto');
+const listCards = document.querySelector('.list-photo');
+const openPhotoPopup = document.querySelector('.popup_open-cards');
+const openImage = openPhotoPopup.querySelector('.popup__open-image');
+const openTitle = openPhotoPopup.querySelector('.popup__open-title');
+const closePopupCardButton = openPhotoPopup.querySelector('.popup__close-open-cards');
+const addFormCard = document.querySelector('.popup__form-cards');
+const nameImage = addFormCard.querySelector('.popup__input_name_image');
+const linkImage = addFormCard.querySelector('.popup__input_link_image');
+const templateCard = document.querySelector('#card-template').content;
 
-const closePopupButton = popup.querySelector('.popup__close');
+const initialCards = [
+  {
+    name: 'Tokyo, Japan',
+    link: 'https://images.unsplash.com/photo-1678951599840-789b26234966?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80',
+  },
+  {
+    name: 'Chenonceaux, France',
+    link: 'https://images.unsplash.com/photo-1679040630230-d6d4bb460493?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    name: 'Molde, Norway',
+    link: 'https://images.unsplash.com/photo-1679068474125-de5bfe7b3518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    name: 'Morocco',
+    link: 'https://images.unsplash.com/photo-1679151009294-f89d3f7b849c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    name: 'Shokawa River, Japan',
+    link: 'https://images.unsplash.com/photo-1678951598862-013dc02df4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+  },
+  {
+    name: 'Aarhus, Denmark',
+    link: 'https://images.unsplash.com/photo-1678973788045-b7eb6ae7de08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+];
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -41,96 +78,49 @@ profileEditButton.addEventListener('click', function() {
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-const addPopupPhoto = document.querySelector('.popup_addPhoto');
-const addButtonPhoto = document.querySelector('.profile__add-button');
-const closePopupAddPhotoButton = addPopupPhoto.querySelector('.popup__close-addphoto');
-
-const listCards = document.querySelector('.list-photo');
-
-const initialCards = [
-  {
-    name: 'Tokyo, Japan',
-    link: 'https://images.unsplash.com/photo-1678951599840-789b26234966?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80',
-  },
-  {
-    name: 'Chenonceaux, France',
-    link: 'https://images.unsplash.com/photo-1679040630230-d6d4bb460493?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-  },
-  {
-    name: 'Molde, Norway',
-    link: 'https://images.unsplash.com/photo-1679068474125-de5bfe7b3518?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    name: 'Morocco',
-    link: 'https://images.unsplash.com/photo-1679151009294-f89d3f7b849c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    name: 'Shokawa River, Japan',
-    link: 'https://images.unsplash.com/photo-1678951598862-013dc02df4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
-  },
-  {
-    name: 'Aarhus, Denmark',
-    link: 'https://images.unsplash.com/photo-1678973788045-b7eb6ae7de08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-];
-
 addButtonPhoto.addEventListener('click', function() {
   popupOpen(addPopupPhoto);
 });
 
 closePopupAddPhotoButton.addEventListener('click', function() {
   popupClose(addPopupPhoto);
+
+  nameImage.value = '';
+  linkImage.value = '';
 });
 
-function createCards(card) {
-  const templateCard = document.querySelector('#card-template').content.cloneNode(true);
-  const imageCard = templateCard.querySelector('.list-photo__image');
-  const titleCard = templateCard.querySelector('.list-photo__title');
-  const cardDelete = templateCard.querySelector('.list-photo__delete-button');
+function createCard(card) {
+  const template = templateCard.querySelector('.list-photo__card').cloneNode(true);
+  const imageCard = template.querySelector('.list-photo__image');
+  const titleCard = template.querySelector('.list-photo__title');
+  const cardDelete = template.querySelector('.list-photo__delete-button');
 
-  const openPhotoPopup = document.querySelector('.popup_open-cards');
-  const openImage = document.querySelector('.popup__open-image');
-  const openTitle = document.querySelector('.popup__open-title');
-  const closePopupCardsButton = openPhotoPopup.querySelector('.popup__close-open-cards');
-
-  listCards.addEventListener('click', function(evt) {
-    if (evt.target.classList.contains('list-photo__image')) {
-      const titleCard = evt.target.closest('.list-photo__card').querySelector('.list-photo__title').textContent;
-      openImage.src = evt.target.src
-      openTitle.textContent = titleCard;
-      popupOpen(openPhotoPopup);
-    }
-  });
-
-  closePopupCardsButton.addEventListener('click', function() {
-    popupClose(openPhotoPopup);
+  imageCard.addEventListener('click', function(evt) {
+    const titleCard = evt.target.closest('.list-photo__card').querySelector('.list-photo__title').textContent;
+    const altCard = evt.target.getAttribute('alt');
+    openImage.src = ('src', evt.target.src);
+    openTitle.textContent = titleCard;
+    openImage.setAttribute('alt', altCard);
+    popupOpen(openPhotoPopup);
   });
 
   imageCard.setAttribute('src', card.link);
+  imageCard.setAttribute('alt', card.alt);
   titleCard.textContent = card.name;
 
-  templateCard.querySelector('.list-photo__like').addEventListener('click', (evt) => {
+  template.querySelector('.list-photo__like').addEventListener('click', (evt) => {
     evt.target.classList.toggle('list-photo__like_active');
   });
 
   cardDelete.addEventListener('click', (evt) => evt.target.closest('.list-photo__card').remove());
 
-  return templateCard;
+  return template;
 }
 
-function renderCards(card, listCards) {
-  const templateCard = createCards(card);
+function renderInitialCards(card, listCards) {
+  const templateCard = createCard(card);
   listCards.prepend(templateCard);
 }
-
-initialCards.forEach(function(card) {
-  const elementCard = createCards(card);
-  listCards.appendChild(elementCard);
-});
-
-const addFormCard = document.querySelector('.popup__form-cards');
-const nameImage = addFormCard.querySelector('.popup__input_name_image');
-const linkImage = addFormCard.querySelector('.popup__input_link_image');
 
 function handleFormSubmitCard(evt) {
   evt.preventDefault();
@@ -138,11 +128,21 @@ function handleFormSubmitCard(evt) {
   const linkValue = linkImage.value;
   const newCards = {
     name: nameValue,
-    link: linkValue
+    link: linkValue,
+    alt: nameValue
   };
   addFormCard.reset();
   popupClose(addPopupPhoto);
-  renderCards(newCards, listCards);
+  renderInitialCards(newCards, listCards);
 }
+
+closePopupCardButton.addEventListener('click', function() {
+  popupClose(openPhotoPopup);
+});
+
+initialCards.forEach(function(card) {
+  const elementCard = createCard(card);
+  listCards.appendChild(elementCard);
+});
 
 addFormCard.addEventListener('submit', handleFormSubmitCard);
